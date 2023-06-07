aec592436759b1c67d351432cc370f425724c879
magneum <krakinzkon@gmail.com>
2023-06-07 15:18:44 +0530

    0.0.4

--- src/types/video.d.ts
+++ src/types/video.d.ts
@@ -1,11 +1,14 @@
 declare module "dlVideoWithAudio" {
   type Resolution = 144 | 240 | 360 | 480 | 720 | 1080 | 1440 | 2160 | 4320;
+
   interface DownloadOptions {
     url: string;
     foldername?: string;
     filename?: string;
     resolution?: Resolution;
   }
+
   function dlVideoWithAudio(options: DownloadOptions): Promise<void>;
+
   export = dlVideoWithAudio;
 }
