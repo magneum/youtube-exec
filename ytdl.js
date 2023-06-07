02d6d33f4506e44385bd38d45e1202bdd4a9bc74
magneum <krakinzkon@gmail.com>
2023-06-07 16:44:15 +0530

    ytdl-exec: 0.0.7

--- ytdl.js
+++ ytdl.js
@@ -1,19 +1,63 @@
 const { dlAudio, dlVideoWithAudio } = require("ytdl-exec");
 
+// dlAudio variations
 (async () => {
-  const quality = "lowest"; // best
-  const filename = "cutom-audio"; // optional
-  const foldername = "./downloads"; // optional
-  const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  const params = { url, foldername, quality, filename };
-  await dlAudio(params);
+  // Variation 1: No foldername
+  await dlAudio({
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    quality: "lowest",
+    filename: "cutom-audio",
+  });
 })();
 
+// dlAudio variations
 (async () => {
-  const resolution = 144; // Resolution 144 | 240 | 360 | 480 | 720 | 1080 | 1440 | 2160 | 4320
-  const filename = "cutom-video"; // optional
-  const foldername = "./downloads"; // optional
-  const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  const params = { url, foldername, filename, resolution };
-  await dlVideoWithAudio(params);
+  // Variation 2: No filename
+  await dlAudio({
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    foldername: "downloads",
+    quality: "lowest",
+  });
+})();
+
+// dlAudio variations
+(async () => {
+  // Variation 3: Variations of quality
+  await dlAudio({
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    foldername: "downloads",
+    quality: "best",
+    filename: "cutom-audio",
+  });
+})();
+
+// dlVideoWithAudio variations
+(async () => {
+  // Variation 1: No foldername
+  await dlVideoWithAudio({
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    filename: "cutom-video",
+    resolution: 144,
+  });
+})();
+
+// dlVideoWithAudio variations
+(async () => {
+  // Variation 2: No filename
+  await dlVideoWithAudio({
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    foldername: "downloads",
+    resolution: 144,
+  });
+})();
+
+// dlVideoWithAudio variations
+(async () => {
+  // Variation 3: Variations of resolution
+  await dlVideoWithAudio({
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    foldername: "downloads",
+    filename: "cutom-video",
+    resolution: 144,
+  });
 })();
